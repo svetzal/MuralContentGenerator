ea28d88a1dda5722ecdaa5f1786c84699a93e98d
Stacey Vetzal <stacey@vetzal.com>
2024-09-07 08:46:31 -0400

    Initial Commit

---
+++ MuralContentGenerator/MuralContentGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuralContentGeneratorApp.swift
+//  MuralContentGenerator
+//
+//  Created by Stacey Vetzal on 2024-09-07.
+//
+
+import SwiftUI
+
+@main
+struct MuralContentGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
